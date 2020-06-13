e28204d254bc882fd7fddbefc208a5f8c50a2af6
DEEPAK-crypto <deepsdeep45@gmail.com>
2020-06-13 22:12:45 +0530

    removed india.js

--- public/scripts/india.js
+++
@@ -1,8 +0,0 @@
-console.log("script loaded")
-var drop = document.querySelector("#exampleFormControlSelect1");
-console.log(drop.value)
-
-document.querySelector("#button").addEventListener("click", function() {
-
-    console.log(drop.value);
-});
